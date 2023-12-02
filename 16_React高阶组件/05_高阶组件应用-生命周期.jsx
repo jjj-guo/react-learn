d45c8f5d56041e85460d1c062b6c26f745e37f02
jjj-guo <2442655199@qq.com>
2023-12-02 20:36:15 +0800

    feat:react HOC

--- 16_React高阶组件/05_高阶组件应用-生命周期.jsx
+++ 16_React高阶组件/05_高阶组件应用-生命周期.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import Detail from './pages/Detail'
 
+// 比如可以赋给函数组件生命周期，计算组件的渲染时间
 export class App extends PureComponent {
   render() {
     return (
